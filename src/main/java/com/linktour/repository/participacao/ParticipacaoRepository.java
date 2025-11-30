d613555d7027ea740a98032885158cdfd574382b
Luskahz <lucasalvesdemelolins@gmail.com>
2025-11-29 23:54:49 -0300

    [feat] - atualização do banco, incluindo repositorio de participação nos eventos, muitos para muitos

---
+++ src/main/java/com/linktour/repository/participacao/ParticipacaoRepository.java
@@ -0,0 +1,13 @@
+package com.linktour.repository.participacao;
+
+import com.linktour.model.participacoes.ParticipacaoEvento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ParticipacaoRepository extends JpaRepository<ParticipacaoEvento, Long> {
+
+    Optional<ParticipacaoEvento> findByUsuarioIdAndEventoId(Long usuarioId, Long eventoId);
+
+    boolean existsByUsuarioIdAndEventoId(Long usuarioId, Long eventoId);
+}
